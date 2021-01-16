7f9908d08909c9a6515dded73acb6ea34ee8910e
Md. Minhazul Islam <minhazmiraz49@gmail.com>
2021-01-17 01:47:50 +0600

    Second Commit

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -2,22 +2,81 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.counter.value,
+    tags: ["tag1", "tag2", "tag3"],
   };
-  render() {
-    return (
-      <React.Fragment>
-        <span style={{ fontSize: 15 }} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
-        <button className="btn btn-secondary">Increment</button>
-      </React.Fragment>
-    );
+
+  /* constructor() {
+    super();
+    this.clickedCb = this.clickedCb.bind(this);
+  } */
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!!</p>;
+    else
+      return (
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      );
+  }
+
+  clickedCb = (product) => {
+    console.log("Button Clicked", product);
+    this.setState({ value: this.state.value + 1 });
+  };
+
+  getBadgeClasses() {
+    let classes = "badge m-2 ";
+    classes += this.state.value > 0 ? "badge-primary" : "badge-warning";
+    return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "zero" : value;
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        {/* this.props.children */}
+        {/* style and class attr */}
+        <span style={{ fontSize: 15 }} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+
+        {/* Event Handler */}
+        <button
+          onClick={() => this.clickedCb(this)}
+          className="btn btn-secondary"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+
+        {/* List render */}
+        {/* <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul> */}
+
+        {/* custom conditional render */}
+        {this.state.tags.length === 0 && "Please create a new tag!!"}
+
+        {/* conditional render */}
+        {/* this.renderTags() */}
+      </div>
+    );
   }
 }
 
---
+++ src/component/counters.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 4 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  onDelete = (counterId) => {
+    console.log("OnDelete event handler called.", counterId);
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.counters.map((counter) => (
+          <Counter key={counter.id} onDelete={this.onDelete} counter={counter}>
+            <h4>Counter #{counter.id}</h4>
+          </Counter>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
